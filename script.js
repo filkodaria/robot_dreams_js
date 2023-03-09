ff86b99c99f9418183ab99f193a1c74d8198a05a
filkodaria <filko_d@ukr.net>
2023-03-10 00:14:43 +0200

    file edited

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
-let firstName;
-let secondName;
-firstName = prompt('Enter first number');
-secondName = prompt('Enter second number');
+let firstNum;
+let secondNum;
+firstNum = prompt('Enter first number');
+secondNum = prompt('Enter second number');
 
 let sum;
 let diff;
 let prod;
 let quot;
-sum = Number(firstName) + Number(secondName);
-diff = Number(firstName) - Number(secondName);
-prod = Number(firstName) * Number(secondName);
-quot = Number(firstName) / Number(secondName);
 
-alert(`Sum=${sum}, Diff=${diff}, Prod=${prod}, Quot=${quot}`);
+sum = Number(firstNum) + Number(secondNum);
+diff = firstNum - secondNum;
+prod = firstNum * secondNum;
+quot = firstNum / secondNum;
+
+if (
+	firstNum === '' ||
+	secondNum === '' ||
+	isNaN(firstNum) ||
+	isNaN(secondNum)
+) {
+	alert('Please, enter numbers');
+} else if (secondNum == 0) {
+	alert('Second value cannot be null');
+} else if (+firstNum < +secondNum) {
+	if (confirm('Are you sure you want to continue?')) {
+		alert(`Diff = ${diff}`);
+	} 
+} else {
+	alert(`Sum = ${sum}, Diff = ${diff}, Prod = ${prod}, Quot = ${quot}`);
+}
